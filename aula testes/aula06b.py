f967e84f41d4b5bffad5abc31de8efacca42c323
JOAQUIM LOPES DE SOUSA NETO <joakis45@gmail.com>
2023-06-29 13:55:29 -0300

    Exercícios 1 - 4

---
+++ aula testes/aula06b.py
@@ -0,0 +1,8 @@
+algo = input ('Escreva algo:')
+
+
+print = ('O que vc escreveu e um numero:',algo.isnumeric)
+print = ('O que vc escreveu contem letras:',algo.isalpha)
+print = ('O que vc escreveu e alfanumerico:',algo.isalnum)
+print = ('O que vc escreveu tem espaços:',algo.isspace)
+
---
+++ exercicios/ex004.py
@@ -0,0 +1,7 @@
+u = input('Escreva algo:')
+
+print('Seu tipo primitivo é:',type(u))
+#print('O que vc escreveu e um numero:',u.isnumeric())
+#print('O que vc escreveu contem letras:',u.isalpha())
+#print('O que vc escreveu e alfanumerico:',u.isalnum())
+#print('O que vc escreveu tem espaços:',u.isspace())
