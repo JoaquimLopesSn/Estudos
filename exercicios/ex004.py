80f11bf99fae63805a905b554d65e6f9d3cb5b76
JOAQUIM LOPES DE SOUSA NETO <joakis45@gmail.com>
2023-06-29 14:01:29 -0300

    Update ex004.py

--- exercicios/ex004.py
+++ exercicios/ex004.py
@@ -1,7 +1,7 @@
 u = input('Escreva algo:')
 
 print('Seu tipo primitivo é:',type(u))
-#print('O que vc escreveu e um numero:',u.isnumeric())
-#print('O que vc escreveu contem letras:',u.isalpha())
-#print('O que vc escreveu e alfanumerico:',u.isalnum())
-#print('O que vc escreveu tem espaços:',u.isspace())
+print('O que vc escreveu e um numero:',u.isnumeric())
+print('O que vc escreveu contem letras:',u.isalpha())
+print('O que vc escreveu e alfanumerico:',u.isalnum())
+print('O que vc escreveu tem espaços:',u.isspace())
