c707b55417b6ad3cbfcce29db6c87223074c0b51
JOAQUIM LOPES DE SOUSA NETO <joakis45@gmail.com>
2023-06-29 23:05:15 -0300

    Aulas 5 - 7

---
+++ exercicios/ex006.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um número:'))
+
+d = n1 * 2
+t = n1 * 3
+ra = n1 ** (1/2)
+
+print('O dobro e de {} vale {}.\nO triplo de {} vale {}.\nA raiz quadrada de {} vale {:.4f}.'.format(n1, d, n1, t, n1, ra))
