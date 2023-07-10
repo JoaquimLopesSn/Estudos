660a0cb24296e87970b9c99a72046283478f7288
JOAQUIM LOPES DE SOUSA NETO <joakis45@gmail.com>
2023-07-10 00:35:17 -0300

    Ex10 e Ex16 atualizações

--- Python/Tratando dados e fazendo contas/Ex010-Conversor de Moedas.py
+++ Python/Tratando dados e fazendo contas/Ex010-Conversor de Moedas.py
@@ -1,3 +1,3 @@
-m = float(input('Digite o valor que voçe tem: '))
+m = float(input('Digite o valor que voçe tem: R$'))
 
 print('Com R${} você pode comprar US${:.2f}'.format(m, m/4.99))
---
+++ Python/Usando modulos do Python/Ex016-Quebrando um número.py
@@ -0,0 +1,6 @@
+import math
+Numero = float(input("Digite um numero que não seja inteiro: "))
+
+numarre = round (Numero)
+
+print (numarre)
