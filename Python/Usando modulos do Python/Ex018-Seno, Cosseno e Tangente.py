540857cf53a6c66f490b03f422c40c210be2e9e5
JOAQUIM LOPES DE SOUSA NETO <joakis45@gmail.com>
2023-07-17 23:45:59 -0300

    Update Ex018-Seno, Cosseno e Tangente.py

--- Python/Usando modulos do Python/Ex018-Seno, Cosseno e Tangente.py
+++ Python/Usando modulos do Python/Ex018-Seno, Cosseno e Tangente.py
@@ -5,6 +5,7 @@ se = math.sin(rad)
 cos = math.cos(rad)
 tan = math.tan(rad)
 
+
 print('O angulo de {} tem o SENO de {:.2f}'.format(ang,se))
 print('O angulo de {} tem o COSSENO de {:.2f}'.format(ang,cos))
 print('O angulo de {} tem o TANGENTE de {:.2f}'.format(ang,tan))
